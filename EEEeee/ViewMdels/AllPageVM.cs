d8a16820c0d8be2029324af092ac679f26d30449
Анна Валитова <anavalitova0@gmail.com>
2022-11-17 21:23:44 +1000

    добавление кнопок, и более менее стало понятно, что делать дальше)

--- EEEeee/ViewMdels/AllPageVM.cs
+++ EEEeee/ViewMdels/AllPageVM.cs
@@ -1,5 +1,7 @@
-﻿using AAA.ViewModels;
+﻿using AAA.Services;
+using AAA.ViewModels;
 using EEEeee.Services;
+using EEEeee.Views;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,9 +14,16 @@ namespace EEEeee.ViewMdels
     {
         private readonly CurrentPageControl currentPageControl;
 
+        public CustomCommand Exit { get; set; }
+
         public AllPageVM(CurrentPageControl currentPageControl)
         {
             this.currentPageControl = currentPageControl;
+
+            Exit = new CustomCommand(() =>
+            {
+                currentPageControl.SetPage(new HeroPage(currentPageControl));
+            });
         }
     }
 }
--- EEEeee/ViewMdels/HeroVM.cs
+++ EEEeee/ViewMdels/HeroVM.cs
@@ -9,6 +9,7 @@ using System.Net.Http;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using System.Windows;
 using WebApplication5.Models;
 
 namespace EEEeee.ViewMdels
@@ -17,11 +18,14 @@ namespace EEEeee.ViewMdels
     {
         public CustomCommand OpenBattle { get; set; }
         public CustomCommand GetHero { get; set; }
-        public CustomCommand GetRoom { get; set; }
+        //public CustomCommand GetRoom { get; set; }
+        public CustomCommand CreateNewRoom { get; set; }
+        public CustomCommand SelectHero { get; set; }
 
         private readonly CurrentPageControl currentPageControl;
 
         public List<Hero> List1 { get; set; } = new List<Hero>();
+        
         
 
         HttpClient client = new HttpClient();
@@ -44,7 +48,6 @@ namespace EEEeee.ViewMdels
 
 
         private Hero selectedHero;
-
         public Hero SelectedHero
         {
             get => selectedHero;
@@ -62,9 +65,10 @@ namespace EEEeee.ViewMdels
             OpenBattle = new CustomCommand(() => {
                 currentPageControl.SetPage(new AllPage(currentPageControl)); });
 
+
             GetHero = new CustomCommand(async() =>
             {
-                var response = await client.GetAsync("http://localhost:5027/api/Heroes");
+                var response = await client.GetAsync("http://localhost:5027/api/SelectHero");
                 IEnumerable<Hero> array;
                 using (var fs = response.Content.ReadAsStream())
                 {
@@ -77,6 +81,23 @@ namespace EEEeee.ViewMdels
 
             GetHero.Execute(null);
 
+            SelectHero = new CustomCommand(() => 
+            {
+                if (SelectedHero == null)
+                {
+                    MessageBox.Show("ВЫБЕРИ ГЕРОЯ !", "ОШИБКА", MessageBoxButton.YesNo, MessageBoxImage.Error);
+                    return;
+                }
+                else
+                {
+                    MessageBox.Show("Всё окс !", "ОШИБКА", MessageBoxButton.YesNo, MessageBoxImage.Error);
+                }
+            });
+
+            CreateNewRoom = new CustomCommand(() =>
+            {
+                MessageBox.Show("Комната успешно создана!");
+            });
             
         }
     }
